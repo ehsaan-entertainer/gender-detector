4047c9cdd3326b5429319dd070466644a058bd1f
Ehsaan Israr <ehsaani@theentertainerasia.com>
2020-10-09 18:41:04 +0500

    added data

--- gender_detector/__init__.py
+++ gender_detector/__init__.py
@@ -2,5 +2,5 @@ from gender_detector import *
 
 __copyright__ = 'Copyright (c) 2014 Marcos Vanetta'
 
-version = (0, 1, 0)
+version = (0, 1, 1)
 version_string = "Gender Detector version %d.%d.%d" % version
